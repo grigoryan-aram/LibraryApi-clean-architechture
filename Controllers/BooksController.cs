06b58dbea575983d2fa0b5034f782daf680199f0
Aram Grigoryan <aramgrigoryan1107@gmail.com>
2026-08-01 16:19:06 +0400

    Initial commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using Application.Features.Books.Commands;
+using Application.Features.Books.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LibraryApi.Controllers;
+
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+
+    private readonly ISender _sender;
+
+
+    public BooksController(ISender sender)
+    {
+
+        _sender = sender;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetBooks()
+    {
+        var result = await _sender.Send(new GetAllBooksQuery());
+
+        return result.Match(books => Ok(books), errors => Problem(title: errors.First().Description));
+    }
+
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetBookById(int id)
+    {
+        var result = await _sender.Send(new GetBookByIdQuery(id));
+
+        return result.Match(book => Ok(book), errors => Problem(title: errors.First().Description));
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> AddBook(AddBookCommand command)
+    {
+
+        var result = await _sender.Send(command);
+
+        return result.Match(book => Ok(book), errors => Problem(title: errors.First().Description));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBook(int id)
+    {
+        var result = await _sender.Send(new DeleteBookCommand(id));
+
+        return result.Match<IActionResult>(
+    _ => NoContent(),
+    errors => Problem(title: errors.First().Description));
+    }
+
+}
---
+++ MiddleWares/RateLimitPerIPMiddleWare.cs
@@ -0,0 +1,35 @@
+﻿namespace LibraryApi.MiddleWares;
+
+public class RateLimitPerIPMiddleWare
+{
+
+    private readonly RequestDelegate _next;
+
+    private static readonly Dictionary<string, int> _Requests = new();
+
+    public RateLimitPerIPMiddleWare(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var ipAddress = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+
+        if (!_Requests.ContainsKey(ipAddress))
+        {
+            _Requests[ipAddress] = 0;
+        }
+
+        _Requests[ipAddress]++;
+
+        if (_Requests[ipAddress] > 50)
+        {
+            context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+            await context.Response.WriteAsync("Too many requests. Please try again later.");
+            return;
+        }
+        await _next(context);
+    }
+}
+
