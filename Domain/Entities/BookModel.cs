55d92b3ff4872c336d96e9bba91ba1a1bbb4959d
Aram Grigoryan <aramgrigoryan1107@gmail.com>
2026-09-16 09:50:10 +0400

    fix

--- Domain/Entities/BookModel.cs
+++ Domain/Entities/BookModel.cs
@@ -4,17 +4,11 @@ namespace LibraryApi.Domain.Entities;
 public class BookModel
 {
     public int Id { get; set; }
-
     public string Title { get; set; } = string.Empty;
-
     public string Author { get; set; } = string.Empty;
-
     public int TotalCopies { get; set; } = 1;
-
     public int CategoryId { get; set; }
     public CategoryModel? Category { get; set; }
-
-
     public ICollection<LoanModel> Loans { get; set; } = new List<LoanModel>();
 
 }
